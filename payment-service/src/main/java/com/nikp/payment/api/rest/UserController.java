e05ad8681a24fb5692a8e8718c2656d8311a2bfa
subhambgh <subhama.sit@gmail.com>
2022-10-14 18:13:08 -0700

    adding configuration using values.yaml

--- payment-service/src/main/java/com/nikp/payment/api/rest/UserController.java
+++ payment-service/src/main/java/com/nikp/payment/api/rest/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.nikp.payment.api.UserService;
 import com.nikp.payment.domain.PaymentAndUser;
